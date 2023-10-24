3515e9aae1fd0608dfafe1fb8ae0efce04dc3410
joaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2023-10-23 21:35:28 -0300

    Initial commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nome = 'João Gabriel';
+  int idade = 16;
+
+  print('nome : $nome');
+  print('idade: $idade');
+
+  if (idade >= 18) {
+    print('Maioridade');
+  } else {
+    print('Menoridade');
+  }
+}
