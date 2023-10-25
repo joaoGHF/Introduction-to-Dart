8152f3737abe0c793561d1f8973e58a65288c1ec
joaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2023-10-24 21:42:10 -0300

    commit aula 24/10/2023

--- main.dart
+++ main.dart
@@ -1,13 +1,33 @@
 void main() {
-  String nome = 'João Gabriel';
-  int idade = 16;
-
-  print('nome : $nome');
+  int idade = 26;
   print('idade: $idade');
 
-  if (idade >= 18) {
-    print('É maior de idade');
+  if (idade >= 0 && idade <= 12) {
+    print('criança');
+  } else if (idade >= 13 && idade <= 17) {
+    print('adolescente');
+  } else if (idade >= 18 && idade <= 60) {
+    print('adulto');
   } else {
-    print('É menor de idade');
+    print('idoso');
+  }
+
+  print('\n---TEMPERATURA---\n');
+
+  double temperatura = 0.3;
+  print('temperatura: $temperatura');
+
+  if (temperatura >= -30 && temperatura <= 0) {
+    print('muito frio');
+  } else if (temperatura > 0 && temperatura <= 12) {
+    print('frio');
+  } else if (temperatura > 12 && temperatura <= 20) {
+    print('temperado');
+  } else if (temperatura > 20 && temperatura <= 31) {
+    print('quente');
+  } else if (temperatura > 31) {
+    print('muito quente');
+  } else {
+    print('ERRO, valor não deve ser menor que -30');
   }
 }
