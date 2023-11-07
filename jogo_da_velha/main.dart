9f43087eae6e7d70a899fcf23482143cbea0dfe3
joaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2023-11-07 19:45:20 -0300

    Add methods haveWinner() and printTable()

--- jogo_da_velha/main.dart
+++ jogo_da_velha/main.dart
@@ -3,6 +3,7 @@ void main(List<String> args) {
   game.setPosix(1, 1, game.player1);
   game.setPosix(0, 1, game.player2);
 
+  game.printTable();
 }
 
 class JogoDaVelha {
@@ -10,13 +11,13 @@ class JogoDaVelha {
   String player2 = "O";
 
   List<List<String>> table = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
   ];
 
   bool setPosix(int row, int colm, String player) {
-    if (table[row][colm] == "") {
+    if (table[row][colm] == " ") {
       table[row][colm] = player;
       return true;
     } else {
@@ -28,9 +29,43 @@ class JogoDaVelha {
     for (var i = 0; i < table.length; i++) {
       String line = "";
       for (var j = 0; j < table[i].length; j++) {
-         // TODO: continue here, imprimir cada char e depois um separdor de linha
+        line += "[" + table[i][j] + "]";
       }
+      print(line);
     }
   }
 
+  bool haveWinner(String player) {
+    for (var i = 0; i < table.length; i++) {
+      //verifica a linha
+      if (table[i][0] == player &&
+          table[i][1] == player &&
+          table[i][2] == player) {
+        return true;
+      }
+
+      //verifica a coluna
+      if (table[0][i] == player &&
+          table[1][i] == player &&
+          table[2][i] == player) {
+        return true;
+      }
+    }
+
+    // verifica a diagonal 1
+    if (table[0][0] == player &&
+        table[1][1] == player &&
+        table[2][2] == player) {
+      return true;
+    }
+
+    // verifica a diagonal 2
+    if (table[0][2] == player &&
+        table[1][1] == player &&
+        table[2][0] == player) {
+      return true;
+    }
+
+    return false;
+  }
 }
